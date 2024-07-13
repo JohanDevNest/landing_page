6c6f7be7b0b020ae51e26187f9cdc6c9bf32c3f3
Soy Jorge <joranherti96@gmail.com>
2024-07-13 13:29:05 -0500

    agregando la creacion de views

--- lib/router/list_routes.dart
+++ lib/router/list_routes.dart
@@ -1,19 +1,54 @@
 import 'package:go_router/go_router.dart';
 
 import '../ui/layouts/main_layout/main_layout_page.dart';
+import '../ui/views/about_us_view/about_us_view.dart';
+import '../ui/views/contact_view/contact_view.dart';
 import '../ui/views/home_view/home_view.dart';
+import '../ui/views/services_view/about_us_view.dart';
 import 'path_routes.dart';
 
 final listRoutes = [
   GoRoute(
-    name: RoutesName.home,
     path: Routes.home,
     pageBuilder: (context, state) {
       // key: state.pageKey;
       return const NoTransitionPage(
         child: MainLayoutPage(
           child: HomeView(),
-        )
+        ),
+      );
+    },
+  ),
+  GoRoute(
+    path: Routes.aboutUsView,
+    pageBuilder: (context, state) {
+      // key: state.pageKey;
+      return const NoTransitionPage(
+        child: MainLayoutPage(
+          child: AboutUsView(),
+        ),
+      );
+    },
+  ),
+  GoRoute(
+    path: Routes.servicesRoute,
+    pageBuilder: (context, state) {
+      // key: state.pageKey;
+      return const NoTransitionPage(
+        child: MainLayoutPage(
+          child: ServicesView(),
+        ),
+      );
+    },
+  ),
+  GoRoute(
+    path: Routes.contactRoute,
+    pageBuilder: (context, state) {
+      // key: state.pageKey;
+      return const NoTransitionPage(
+        child: MainLayoutPage(
+          child: ContactView(),
+        ),
       );
     },
   ),
--- lib/ui/shared/custom_app_menu/responsive/tablet_desktop.dart
+++ lib/ui/shared/custom_app_menu/responsive/tablet_desktop.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
+import '../../../../router/path_routes.dart';
 import '../widgets/custom_flat_button.dart';
 
 class TabletDesktopMenu extends StatelessWidget {
@@ -31,28 +33,36 @@ class TabletDesktopMenu extends StatelessWidget {
           CustomFlatButton(
             text: 'Inicio',
             // onPressed: () => Navigator.pushNamed(context, '/stateful'),
-            onPressed: () {},
+            onPressed: () {
+               GoRouter.of(context).go(Routes.home);
+            },
             fontSize: 18,
           ),
           const SizedBox(width: 10),
           CustomFlatButton(
             text: 'Acerca de nosotros',
             // onPressed: () => Navigator.pushNamed(context, '/provider'),
-            onPressed: () {},
+            onPressed: () {
+              GoRouter.of(context).go(Routes.aboutUsView);
+            },
             fontSize: 18,
           ),
           const SizedBox(width: 10),
           CustomFlatButton(
             text: 'Servicios',
             // onPressed: () => Navigator.pushNamed(context, '/abc123'),
-            onPressed: () {},
+            onPressed: () {
+              GoRouter.of(context).go(Routes.servicesRoute);
+            },
             fontSize: 18,
           ),
           const SizedBox(width: 10),
           CustomFlatButton(
             text: 'Contactanos',
             // onPressed: () => Navigator.pushNamed(context, '/stateful/100'),
-            onPressed: () {},
+            onPressed: () {
+                 GoRouter.of(context).go(Routes.contactRoute);
+            },
             backGroundColor: const Color(0xFF6D11B4),
             fontSize: 18,
           ),
