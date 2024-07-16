d85dc582b6752b4446353a19fad1f0c22caa5429
Soy Jorge <joranherti96@gmail.com>
2024-07-16 08:44:08 -0500

    eliminando los routername por que no se van a usar

--- lib/router/path_routes.dart
+++ lib/router/path_routes.dart
@@ -8,19 +8,5 @@ class Routes {
   static const servicesRoute = '/services';
   static const contactRoute = '/contact';
 
-  // Dashboard
-  static const dashboardRoute = '/dashboard';
-  static const splashRoute = '/splash';
 }
 
-class RoutesName {
-  // Auth Router
-
-  static const home = '/';
-  static const loginRoute = 'login';
-  static const registerRoute = 'register';
-
-  // Dashboard
-  static const dashboardRoute = 'dashboard';
-  static const splashRoute = 'splash';
-}
