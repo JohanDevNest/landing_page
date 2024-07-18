55f18f580ce28e6635b7d21077c23f42ab9e7975
Johan Vargas <johansantiago56@gmail.com>
2024-07-18 15:47:05 -0500

    añadiendo scrollcontontroller para poder modificar el color del custom app menu, pm(homeview,mainlayoutpage,customappmenu,tabletdesktop,listroutes

--- lib/ui/views/home_view/home_view.dart
+++ lib/ui/views/home_view/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../layouts/main_layout/main_layout_page.dart';
 import 'sections/about_section.dart';
 import 'sections/advantages_section.dart';
 import 'sections/banner.dart';
@@ -8,23 +9,37 @@ import 'sections/hero_section.dart';
 import 'sections/our_technologies.dart';
 import 'sections/services_section.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
   @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
   Widget build(BuildContext context) {
-    return ListView(
-      scrollDirection: Axis.vertical,
-      //
-      children: const [
-        HeroSection(),
-        OurTechnologies(),
-        ServicesSection(),
-        AboutSection(),
-        BannerSection(),
-        AdvantagesSection(),
-        FooterSection()
-      ],
+    return NotificationListener<ScrollNotification>(
+      onNotification: (ScrollNotification scrollInfo) {
+        if (scrollInfo.metrics.axis == Axis.vertical) {
+          final MainLayoutPageState? layoutPageState =
+              context.findAncestorStateOfType<MainLayoutPageState>();
+          layoutPageState?.updateScrollPosition(scrollInfo.metrics.pixels);
+        }
+        return true;
+      },
+      child: ListView(
+        scrollDirection: Axis.vertical,
+        children: const [
+          HeroSection(),
+          OurTechnologies(),
+          ServicesSection(),
+          AboutSection(),
+          BannerSection(),
+          AdvantagesSection(),
+          FooterSection(),
+        ],
+      ),
     );
   }
 }
